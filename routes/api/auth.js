15b876ed0f471a0ab22b8b5f1d6d744ce0049237
Omar Miah <omiahcs@gmail.com>
2020-12-28 19:36:36 -0500

    Middleware for secure routing

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,16 +1,28 @@
 //auth.js
 const express = require('express'); // bringing in express
+const { restart } = require('nodemon');
 
 //to use the express router
 const router = express.Router();
+const auth = require('../../middleware/auth'); // to make my routes secure 
+const Users = require('../../models/Users');
 
+const User = require('../../models/Users') // to get the user's info
 // when we want to create a route we have to do
 
 // @route         GET api/auth
 // @description   Test route
 // @access        public
 
-router.get('/', (req, res) => res.send('auth route')); // this sends a response to the server
+router.get('/', auth, async (req, res) => {
+   try {
+       const user = await User.findById(req.user.id).select('-password');
+       res.json(user);
+   }catch(err){
+       console.error(err.message);
+       res.status(500).send('Server Error');
+   }
+}); // this sends a response to the server
 // that prints the message 'auth route'
 // then we want to export this module
 
